bb8b6a21cfbae6d131e33406eeba90a11fada9cd
Evgeniy Kelyarsky <kelyar@gmail.com>
2015-04-19 00:20:33 +0300

    downloads xlsx instead of rendering html table

--- xls-time.rb
+++ xls-time.rb
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 require 'rubyXL'
 require 'sinatra'
 require 'date'
@@ -18,7 +20,7 @@ post '/upload' do
     dat, point, name = row
 
     @employees[name] ||= {} 
-    day = dat.strftime("%Y%m%d")
+    day = dat.strftime("%m/%d/%Y")
 
     @employees[name][day] ||= [dat, dat] # initial value: start = quit
     if dat > @employees[name][day].last # more recent activity, update quit time
@@ -26,5 +28,19 @@ post '/upload' do
     end
   end
 
-  erb :result
+  i = 0
+  worksheet = timetable.add_worksheet('По дням')
+  @employees.each do |name, days|
+    days.each do |day, times|
+      worksheet.add_cell i, 0, day
+      worksheet.add_cell i, 1, times[0].strftime('%H:%M')
+      worksheet.add_cell i, 2, times[1].strftime('%H:%M')
+      worksheet.add_cell i, 3, name
+      i += 1
+    end
+  end
+  # TODO: stream
+  fname = "new-#{params['myfile'][:filename]}"
+  timetable.write("processed/#{fname}")
+  send_file "processed/#{fname}", filename: fname
 end
