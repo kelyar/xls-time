a28da0c93226fa6897a837c84e258c351c749bd8
Evgeniy Kelyarsky <kelyar@gmail.com>
2015-04-19 20:55:30 +0300

    missing days added

--- xls-time.rb
+++ xls-time.rb
@@ -29,14 +29,28 @@ post '/upload' do
     end
   end
 
+  @employees.each do |name, days|
+    current_day = days.first[1][0]
+    last_day = days.to_a.last[1][0] # dirty way to get last day
+
+    while current_day < last_day do
+      index = current_day.strftime("%m/%d/%Y")
+      @employees[name][index] ||= false
+      current_day = current_day.next
+    end
+    @employees[name] = @employees[name].sort_by {|day,times| DateTime.strptime day, '%m/%d/%Y' }
+  end
+
   i = 0
   worksheet = timetable.add_worksheet('По дням')
   @employees.each do |name, days|
     days.each do |day, times|
       worksheet.add_cell i, 0, day
-      worksheet.add_cell i, 1, times[0].strftime('%H:%M')
-      worksheet.add_cell i, 2, times[1].strftime('%H:%M')
-      worksheet.add_cell i, 3, name
+      if times
+        worksheet.add_cell i, 1, times[0].strftime('%H:%M')
+        worksheet.add_cell i, 2, times[1].strftime('%H:%M')
+        worksheet.add_cell i, 3, name
+      end
       i += 1
     end
   end
